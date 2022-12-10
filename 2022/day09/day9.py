15ddd61ec86711788a2860187e7ab819730c4b87
GitCass01 <72577093+GitCass01@users.noreply.github.com>
2022-12-10 13:07:32 +0100

    solution for day9

--- 2022/day09/day9.py
+++ 2022/day09/day9.py
@@ -48,33 +48,36 @@ def part1(lines: list[str]):
             pos.add((posT[0], posT[1]))
     return len(pos)
 
-def part2(lines: list[str], num=10):
-    lst = [0, 0]
-    posK = {i: [0, 0] for i in range(num)}
-    pos_tail = set()
+# parte 2 rifatta da zero
+def mov_t(dist):
+    if dist[0] in [-1,0,1] and dist[1] in [-1,0,1]: return (0,0)
+    if dist[0] > 1 and dist[1]==0: return (1,0)
+    if dist[0] < -1 and dist[1]==0: return (-1,0)
+    if dist[1] > 1 and dist[0]==0: return (0,1)
+    if dist[1] <-1 and dist[0]==0: return (0,-1)
+    if dist[0] > 0 and dist[1] > 0: return (1,1)
+    if dist[0] < 0 and dist[1] > 0: return (-1,1)
+    if dist[0] > 0 and dist[1] < 0: return (1,-1)
+    if dist[0] <0 and dist[1] < 0: return (-1,-1)
+
+def add_mov(pos, mov):
+    pos[0] += mov[0]
+    pos[1] += mov[1]
+    return pos
+
+def part2(lines: list[str]):
+    pos = [[0,0] for x in range (10)]
+    pos_tail = set([(0,0)])
+    mov_dict = { 'L':(-1,0), 'R':(1,0), 'D':(0,-1), 'U':(0,1)}   
     
-    for line in lines:
-        c, n = line.split(' ')
-        for _ in range(0, int(n)):
-            prec = posK[0].copy()
-            prec2 = posK[1].copy()
-            match c:
-                case 'R':
-                    posK[0][1] += 1
-                case 'L':
-                    posK[0][1] -= 1
-                case 'U':
-                    posK[0][0] += 1
-                case 'D':
-                    posK[0][0] -= 1
-            for i in range(1, num):
-                if i != 1:
-                    prec2 = posK[i].copy()
-                if not checkAdjacency(posK[i - 1], posK[i]):
-                    posK[i] = prec.copy()
-                prec = prec2.copy()
-            print(posK[num-1])
-            pos_tail.add((posK[num - 1][0], posK[num - 1][1]))
+    for l in lines:
+        l = l.split(' ')
+        for n in range (int(l[1])):
+            pos[0] = add_mov(pos[0], mov_dict[l[0]])
+            for i in range (1, len(pos)):
+                dist = (pos[i-1][0] - pos[i][0], pos[i-1][1] - pos[i][1])
+                pos[i] = add_mov(pos[i], mov_t(dist))
+            pos_tail.add((pos[9][0],pos[9][1]))
     return len(pos_tail)
 
 def approx(seconds: float):
@@ -95,7 +98,7 @@ print(part1(lines))
 end_time = time.time()
 print("--- {} ---".format(approx(end_time - start_time)))
 
-# start_time = time.time()
+start_time = time.time()
 print(part2(lines))
-# end_time = time.time()
-# print("--- {} ---".format(approx(end_time - start_time)))
+end_time = time.time()
+print("--- {} ---".format(approx(end_time - start_time)))
